6d33838a18142676ca20c7647e246bf9c9141b38
Odd Henry Lindtner <oddhenry@gmail.com>
2022-09-11 20:22:33 +0200

    Viser antall søketreff i NRK Radio

---
+++ Odd-Henry-search.py
@@ -0,0 +1,22 @@
+import requests
+from datetime import datetime
+
+search_string = "Odd Henry Lindtner"
+print(f"Searching for {search_string}")
+search_string = search_string.replace(" ", "+")
+
+r = requests.get(f'https://psapi.nrk.no/radio/search/search?q="{search_string}"')
+result = r.json()
+print(f"got {result['count']} results")
+
+for episode in result['results']['episodes']['results']:
+    date = datetime.fromisoformat(episode['date'])
+    #print(episode['images'])
+
+    print(f"got episode '{episode['title']}' from {date.strftime('%A %d. %B %Y kl. %H:%M')}.")
+    
+    #It has the following highlights:
+    #for highlight in episode['highlights']:
+    #    print(f"  {highlight['text']}")
+
+
